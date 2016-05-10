c1f4c24a9960fe901fae74d6c21eeddc53156153
Anton Bondarenko <bond.anton@gmail.com>
2016-05-10 18:44:32 +0300

    Parameters data structure refactored.

--- ScientificProjects/Entities/Equipment.py
+++ ScientificProjects/Entities/Equipment.py
@@ -4,18 +4,32 @@ from sqlalchemy import Table, Column, Integer, String, Text, DateTime, ForeignKe
 from sqlalchemy.orm import relationship, backref
 
 from ScientificProjects import Base
-from ScientificProjects.Entities.Project import Project
 from ScientificProjects.Entities.User import User
 from ScientificProjects.Entities.Parameter import Parameter
+from ScientificProjects.Entities.MeasurementType import MeasurementType
 
 
-association_table = Table('assembly_parts', Base.metadata,
-                          Column('assembly_id', Integer, ForeignKey('equipment_assembly.id')),
-                          Column('equipment_id', Integer, ForeignKey('equipment.id')))
+equipment_assembly_table = Table('assembly_parts', Base.metadata,
+                                 Column('assembly_id', Integer, ForeignKey('equipment_assembly.id')),
+                                 Column('equipment_id', Integer, ForeignKey('equipment.id')))
 
 equipment_parameters_table = Table('equipment_parameters', Base.metadata,
                                    Column('equipment_id', Integer, ForeignKey('equipment.id')),
                                    Column('parameter_id', Integer, ForeignKey('parameter.id')))
+
+equipment_measurement_table = Table('equipment_measurement', Base.metadata,
+                                    Column('equipment_id', Integer, ForeignKey('equipment.id')),
+                                    Column('measurement_type_id', Integer, ForeignKey('measurement_type.id')))
+
+
+class Manufacturer(Base):
+
+    __tablename__ = 'manufacturer'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True)
+    abbreviation = Column(String)
+    description = Column(Text)
+    created = Column(DateTime, default=func.now())
 
 
 class EquipmentCategory(Base):
@@ -34,16 +48,23 @@ class Equipment(Base):
     __tablename__ = 'equipment'
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
+    manufacturer_id = Column(Integer, ForeignKey('manufacturer.id'))
+    manufacturer = relationship('Manufacturer', backref=backref('equipment', uselist=True,
+                                                                cascade='delete,all'))
     category_id = Column(Integer, ForeignKey('equipment_category.id'))
     category = relationship(EquipmentCategory, backref=backref('equipment', uselist=True,
                                                                cascade='delete,all'))
     assembly_id = Column(Integer, ForeignKey('equipment_assembly.id'))
     assembly = relationship('EquipmentAssembly', backref=backref('equipment', uselist=True,
-                                                               cascade='delete,all'))
+                                                                 cascade='delete,all'))
+    inventory_number = Column(String)
+    serial_number = Column(String)
+    version = Column(String)
     description = Column(Text)
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User, backref=backref('equipment', uselist=True, cascade='delete,all'))
     parameters = relationship(Parameter, secondary=equipment_parameters_table, backref="equipment")
+    measurements = relationship(MeasurementType, secondary=equipment_measurement_table, backref="equipment")
     created = Column(DateTime, default=func.now())
 
     def __str__(self):
@@ -56,7 +77,7 @@ class EquipmentAssembly(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
     description = Column(Text)
-    parts = relationship(Equipment, secondary=association_table, backref="assemblies")
+    parts = relationship(Equipment, secondary=equipment_assembly_table, backref="assemblies")
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User, backref=backref('assemblies', uselist=True, cascade='delete,all'))
     created = Column(DateTime, default=func.now())
