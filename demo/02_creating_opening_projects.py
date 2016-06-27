9dbf6ad7c14330de4a701310c9f8769a1ef1845b
Anton Bondarenko <bond.anton@gmail.com>
2016-06-27 21:39:22 +0300

    Demo fixed.

--- demo/02_creating_opening_projects.py
+++ demo/02_creating_opening_projects.py
@@ -7,7 +7,9 @@ client = Client(config_file_name='config.ini')
 client.user_manager.sign_in('john_smith', 'secret_password')
 
 project_name = 'Super Project'
-client.user_manager.project_manager.create_project(project_name, 'My first ever really super project', 'data/files')
+client.user_manager.project_manager.create_project(name=project_name,
+                                                   description='My first ever really super project',
+                                                   data_dir='data/files')
 client.user_manager.project_manager.open_project(project_name)
 client.user_manager.project_manager.close_project()
 
