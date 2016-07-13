7be8dc8e8324043f8da5168eda59ad375e8c7a8a
Anton Bondarenko <bond.anton@gmail.com>
2016-07-13 17:31:40 +0300

    testing parameters comparison

--- ScientificProjects/Entities/Parameter.py
+++ ScientificProjects/Entities/Parameter.py
@@ -95,6 +95,7 @@ class Parameter(Base):
                 matched = False
                 for other_child in other.children:
                     if child.equals(other_child):
+                        print('\t', child.name, 'equals', other_child.name)
                         matched = True
                         break
                 if not matched:
