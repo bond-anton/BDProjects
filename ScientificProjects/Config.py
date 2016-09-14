0327c3e12ed13b0ad3cf1c0c62c5d0887c31d802
Anton Bondarenko <bond.anton@gmail.com>
2016-09-14 13:27:45 +0300

    Config file handling fixes.

--- ScientificProjects/Config.py
+++ ScientificProjects/Config.py
@@ -1,5 +1,6 @@
 from __future__ import division, print_function
 import sys
+import numbers
 try:
     import configparser
 except ImportError:
@@ -40,6 +41,13 @@ def read_config(file_name=None):
 
 
 def write_config(connection_parameters, file_name):
+    if isinstance(connection_parameters['port'], numbers.Number):
+        if connection_parameters['port'] == 0:
+            port_string = ''
+        else:
+            port_string = '%d' % connection_parameters['port']
+    else:
+        port_string = ''
     config = configparser.ConfigParser()
     config.add_section('Database')
     if sys.version_info.major > 2:
@@ -47,14 +55,14 @@ def write_config(connection_parameters, file_name):
         db_config['name'] = connection_parameters['db_name']
         db_config['backend'] = connection_parameters['backend']
         db_config['host'] = connection_parameters['host']
-        db_config['port'] = connection_parameters['port']
+        db_config['port'] = port_string
         db_config['user'] = connection_parameters['user']
         db_config['password'] = connection_parameters['password']
     else:
         config.set('Database', 'name', connection_parameters['db_name'])
         config.set('Database', 'backend', connection_parameters['backend'])
         config.set('Database', 'host', connection_parameters['host'])
-        config.set('Database', 'port', str(connection_parameters['port']))
+        config.set('Database', 'port', port_string)
         config.set('Database', 'user', connection_parameters['user'])
         config.set('Database', 'password', connection_parameters['password'])
     with open(file_name, 'w') as configfile:
