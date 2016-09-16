9230a20a99c79a37658635791589c05b278e956b
Anton Bondarenko <bond.anton@gmail.com>
2016-09-16 12:34:52 +0300

    Handling config file Section or Option not found exception.

--- ScientificProjects/Config.py
+++ ScientificProjects/Config.py
@@ -18,28 +18,31 @@ def read_config(file_name=None):
             raise IOError('Config file not found')
         config = configparser.ConfigParser()
         config.read(file_name)
-        if sys.version_info.major > 2:
-            db_config = config['Database']
-            db_name = db_config['name']
-            backend = db_config['backend']
-            hostname = db_config['host']
-            port = db_config['port']
-            user = db_config['user']
-            password = db_config['password']
-        else:
-            db_name = config.get('Database', 'name')
-            backend = config.get('Database', 'backend')
-            hostname = config.get('Database', 'host')
-            port = config.get('Database', 'port')
-            user = config.get('Database', 'user')
-            password = config.get('Database', 'password')
-        connection_parameters = {'db_name': db_name,
-                                 'backend': backend,
-                                 'host': hostname,
-                                 'port': port,
-                                 'user': user,
-                                 'password': password
-                                 }
+        try:
+            if sys.version_info.major > 2:
+                db_config = config['Database']
+                db_name = db_config['name']
+                backend = db_config['backend']
+                hostname = db_config['host']
+                port = db_config['port']
+                user = db_config['user']
+                password = db_config['password']
+            else:
+                db_name = config.get('Database', 'name')
+                backend = config.get('Database', 'backend')
+                hostname = config.get('Database', 'host')
+                port = config.get('Database', 'port')
+                user = config.get('Database', 'user')
+                password = config.get('Database', 'password')
+            connection_parameters = {'db_name': db_name,
+                                     'backend': backend,
+                                     'host': hostname,
+                                     'port': port,
+                                     'user': user,
+                                     'password': password
+                                     }
+        except(configparser.NoSectionError, configparser.NoOptionError):
+            raise ValueError('Section or Option not found in config file')
     return connection_parameters
 
 
